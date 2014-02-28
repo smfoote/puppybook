6200182c5b1a27c5d94d46248ec4e81583528bca
Steven Foote <sfoote@linkedin.com>
2014-02-28 05:03:46 -0800

    merge chaper 6

--- js/prompt.js
+++ js/prompt.js
@@ -1,5 +1,10 @@
 // Get the user's name.
 var userName = prompt('Hello, what\'s your name?');
+
+if (!userName) {
+  prompt('You didn\'t enter a name. Really, what\'s your name?');
+}
+
 // Get the user's phone number.
 var phoneNumber = prompt('Hello ' + userName +', what\'s your phone number?');
 // Create the phone number pattern.
@@ -9,26 +14,26 @@ var phoneMatches = phoneNumberPattern.exec(phoneNumber);
 // Create a variable to store the output.
 var output = '<h1>Hello, ' + userName + '!</h1>';
 // Create other variables to be used later
-var areaCode, location;
+var areaCode, locationName;
 
 // Is the phone number valid?
 if (phoneMatches) {
 
-	// Yes, the phone number is valid!
-	// Get area code
-	areaCode = phoneMatches[1];
-	// Get location
-	location = pbValues.areaCodes[areaCode];
-	if (location) {
-		output += '<h2>How is ' + pbValues.areaCodes[phoneMatches[1]] +
-							' this time of year?</h2>';
-	}
+  // Yes, the phone number is valid!
+  // Get area code
+  areaCode = phoneMatches[1];
+  // Get location
+  locationName = pbValues.areaCodes[areaCode];
+  if (locationName) {
+    output += '<h2>How is ' + locationName +
+              ' this time of year?</h2>';
+  }
   output = output + '<p>' + pbValues.projectName + ' ' +
            pbValues.versionNumber +' viewed on: ' + pbValues.currentTime + '</p>';
 
 } else {
 
-	// No, the phone number is not valid. Tell the user about the problem.
+  // No, the phone number is not valid. Tell the user about the problem.
   output = output + '<h2>That phone number is invalid: ' + phoneNumber;
 }
 
