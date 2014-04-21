0be998c79160e752a7e526442ad47b89ff94ff0f
Steven Foote <sfoote@linkedin.com>
2014-04-21 13:35:13 -0700

    unit tests

--- release/main.js
+++ release/main.js
@@ -10,6 +10,11 @@ var pbValues = {
   }
 };
 
+/**
+ * Get the area codes defined in pbValues
+ * @method
+ * @return {object}
+ */
 function getAreaCodes() {
   return pbValues.areaCodes;
 }
@@ -35,10 +40,15 @@ function getUserName() {
  * @return {boolean}
  */
 function validatePhoneNumber(phoneNumber) {
-  return !! phoneNumber.match(/(?:1-)?\(?(\d{3})[\-\)]\d{3}-\d{4}/);
+  return (/(?:1-)?\(?(\d{3})(?:-|\) ?)\d{3}-\d{4}/).test(phoneNumber);
 }
 
-// Get the user's phone number.
+/**
+ * Ask the user for their phone number. Twice if necessary.
+ * @method
+ * @param {string} userName The user's name, so we can ask the user by name
+ * @return {string}
+ */
 function getPhoneNumber(userName) {
   var phoneNumber = prompt('Hello ' + userName +', what\'s your phone number?');
   if (!validatePhoneNumber(phoneNumber)) {
@@ -47,6 +57,14 @@ function getPhoneNumber(userName) {
   return phoneNumber;
 }
 
+/**
+ * Return a geographic location (in the form of a string),
+ * based on a phone number
+ * @method
+ * @param {string} phoneNumber The phone number to be used to get the location.
+ * It is assumed that this phone number has already been validated.
+ * @return {string} The geographic location of the phone number's area code
+ */
 function getLocation(phoneNumber) {
   // Create the phone number pattern.
   var phoneNumberPattern = /(?:1-)?\(?(\d{3})[\-\)]\d{3}-\d{4}/;
@@ -61,18 +79,43 @@ function getLocation(phoneNumber) {
   return locationName ? locationName : 'somewhere';
 }
 
+/**
+ * Get the images in the Facebook news feed
+ * @method
+ * @return {array} An array-like collection of HTML elements
+ */
 function getImages() {
   var images = document.querySelectorAll('div.userContentWrapper img');
   return images;
 }
+
+/**
+ * Get the height of the given image
+ * @method
+ * @param {image} image An image, either a DOM reference to an <img> or `new Image`
+ * @return {number}
+ */
 function getImageHeight(image) {
   return image.height;
 }
 
+/**
+ * Get the width of the given image
+ * @method
+ * @param {image} image An image, either a DOM reference to an <img> or `new Image`
+ * @return {number}
+ */
 function getImageWidth(image) {
   return image.width;
 }
 
+/**
+ * Change the source of all of the given images. The new source depends on the
+ * given location.
+ * @method
+ * @param {array} images An array of Images
+ * @param {string} location A string that represents a location.
+ */
 function replaceImages(images, location) {
   var baseImageUrl, height, width, image;
   switch (location) {
@@ -80,7 +123,7 @@ function replaceImages(images, location) {
     // Use puppies for Memphis
     baseImageUrl = 'http://placepuppy.it/';
     break;
-  default:
+  case 'Utah':
     // use kittens everywhere else
     baseImageUrl = 'http://placekitten.com/';
     break;
@@ -93,11 +136,16 @@ function replaceImages(images, location) {
   }
 }
 
+/**
+ * The main method is used to start the program
+ * @method
+ */
 function main() {
   var userName = getUserName();
   var phoneNumber = getPhoneNumber(userName);
   var location = getLocation(phoneNumber);
   var images = getImages();
+  replaceImages(images, location);
   setInterval(function() {
     images = getImages();
     replaceImages(images, location);
